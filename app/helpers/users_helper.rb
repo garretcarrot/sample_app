2b5a77c57f8b04b668bcb2543f26467cce171864
Garret Thompson <garret.thompson@tstllc.net>
2014-11-08 12:52:59 -0500

    Add a Gravatar image to the user profile view

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+
+  # Get a user's Gravatar for use in the profile sidebar.
+  #
+  # user  - The ActiveRecord object containing the email address the image
+  #         is associated with.
+  #
+  # Returns the image tag of the Gravatar for the given user.
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag gravatar_url, alt: user.name, class: 'gravatar'
+  end
 end
