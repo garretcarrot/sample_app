9e1ed9008d9b3007ea6e58541ab8d59f6e875e48
Garret Thompson <garret.thompson@tstllc.net>
2014-11-11 17:08:55 -0500

    Add a 'create' action w/auth and a stub 'destroy'

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Log in & redirect to the user's show page.
+    else
+      # Create an error message.
+    end
+    render 'new'
+  end
+
+  def destroy
+  end
 end
