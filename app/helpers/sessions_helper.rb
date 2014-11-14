5cc5a9a153e10fd52ef9af62dc718cd7e5fd110c
Garret Thompson <garret.thompson@tstllc.net>
2014-11-14 01:14:23 -0500

    Enable login success w/ a temporary session cookie

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,17 @@
 module SessionsHelper
+
+  # Log the given user into the sample application. 
+  # This will expire when the browser window closes.
+  #
+  # Cookies created using `session` are automatically encrypted,
+  # so this method is secure. Permanent cookies created with the
+  # `cookie` method do not have this feature and are susceptible
+  # to attacks.
+  # 
+  # user - The instance of the User model being logged in.
+  #
+  # Returns the user's id.
+  def log_in(user)
+    session[:user_id] = user.id
+  end
 end
