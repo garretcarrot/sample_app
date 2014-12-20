97c2cb1695727218ab464e309e22b31f4dd4e840
Garret Thompson <garret.thompson@tstllc.net>
2014-12-19 23:29:24 -0500

    Add password resets

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -23,6 +27,7 @@ Rails.application.routes.draw do
   #   resources :products
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 
   # Example resource route with options:
   #   resources :products do
